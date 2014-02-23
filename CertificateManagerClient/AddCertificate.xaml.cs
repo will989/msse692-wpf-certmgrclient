363d9fde49c6ce3f11e741d7645fa82aeecb1858
will989 <will989@regis.edu>
2014-02-22 17:28:14 -0700

    Add Certificate to local computer and Save Cert to Warehouse enabled

---
+++ CertificateManagerClient/AddCertificate.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CertificateManagerClient.TestUtilities.TestUtilities;
+
+namespace CertificateManagerClient
+{
+    /// <summary>
+    /// Interaction logic for AddCertificate.xaml
+    /// </summary>
+    public partial class AddCertificate : Page
+    {
+        public AddCertificate()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.CertChooser.Text = ofd.FileName;
+            }
+
+            try
+
+            {
+                //load certificate from selected file
+                X509Certificate2 loadedCertificate = new X509Certificate2(ofd.FileName);
+                System.Diagnostics.Debug.WriteLine("Reading Client Certificate Information");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+        }
+
+        private void AddCertButton_Click(object sender, RoutedEventArgs e)
+        {
+            //instantiate web service
+            CertificateManagerService.CertificateManagerServiceClient
+                 wsref = new CertificateManagerService.CertificateManagerServiceClient();
+
+            // *** FOR TESTING -- NEED TO PASS THESE IN FROM THE PAGE SOMEHOW ***
+            //create a unique subjectName
+            string subjectName = string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}", DateTime.Now);
+
+            X509Store store = new X509Store("CA", StoreLocation.LocalMachine);
+            X509Certificate2 certificate =
+                new X509Certificate2(CreateCertificate.CreateSelfSignedCertificate(subjectName));
+
+           bool added = wsref.InstallCertificateLocal(store, certificate);
+
+            if (added)
+            {
+                System.Diagnostics.Debug.WriteLine("Certificate was added");
+            }
+        }
+    }
+}
--- CertificateManagerClient/CertificateManagerHome.xaml.cs
+++ CertificateManagerClient/CertificateManagerHome.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CertificateManagerClient.CertificateWarehouseService;
 using MessageBox = System.Windows.Forms.MessageBox;
 using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
@@ -26,15 +30,126 @@ namespace CertificateManagerClient
         public CertificateManagerHome()
         {
             InitializeComponent();
+            Item = new Certificate();
         }
         //from here:  http://social.msdn.microsoft.com/Forums/vstudio/en-US/29b08a8a-9879-401d-92d6-181a93f3c792/select-a-file-in-a-wpf-application?forum=wpf
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+
+             System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+
              if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
              {
                  this.CertChooser.Text = ofd.FileName;
              }
-         }
+
+            try
+            {
+               
+                {
+                    //load certificate from selected file
+                    X509Certificate2 loadedCertificate = new X509Certificate2(ofd.FileName);
+                    System.Diagnostics.Debug.WriteLine("Reading Client Certificate Information");
+                    
+                    //null check to make sure certificate was loaded, then populate form
+                    if (loadedCertificate.SubjectName.Name != null) CertName.Text = loadedCertificate.SubjectName.Name;
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Certificate Name was null, certificate not loaded correctly");
+                    }
+                    StartDate.Text = loadedCertificate.NotBefore.ToLongDateString();
+                    EndDate.Text = loadedCertificate.NotAfter.ToLongDateString();
+                    ExpirationDate.Text = loadedCertificate.NotAfter.ToLongDateString();
+                    Thumbprint.Text = loadedCertificate.Thumbprint;
+                    //try storing content this way (http://www.dotnetperls.com/convert-string-byte-array)
+                    CertContent.Text = Encoding.ASCII.GetString(loadedCertificate.RawData);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                //do something
+            }
         }
+
+        public Certificate Item
+
+        {
+            get
+            {
+                var objectDataProvider = FindResource("MyCertificateProvider") as ObjectDataProvider;
+                if (objectDataProvider != null)
+                    return objectDataProvider.ObjectInstance as Certificate;
+                return null;
+            }
+            set
+            {
+                var objectDataProvider = FindResource("MyCertificateProvider") as ObjectDataProvider;
+                if (objectDataProvider != null)
+                    objectDataProvider.ObjectInstance = value;
+            }
+        }
+ 
+
+        private void SaveCertButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //instantiate web service
+            CertificateWarehouseService.CertificateWarehouseServiceClient
+                wsref = new CertificateWarehouseService.CertificateWarehouseServiceClient();
+            
+            //new CertificateManager Certificate
+            Certificate certificate = new Certificate();
+            certificate.Name = CertName.Text;
+            certificate.StartDate = Convert.ToDateTime(StartDate.Text);
+            certificate.EndDate = Convert.ToDateTime(EndDate.Text);
+            if (IsDeleted.IsChecked != null && (bool) IsDeleted.IsChecked)
+            {
+                certificate.IsDeleted = (bool)IsDeleted.IsChecked;  
+            }
+            else
+            {
+                certificate.IsDeleted = false;
+            }
+            certificate.ExpirationDate = Convert.ToDateTime(ExpirationDate.Text);
+            certificate.Thumbprint = Thumbprint.Text;
+            //try converting certificate content from text back to bytes (http://www.dotnetperls.com/convert-string-byte-array)
+            certificate.Content = Encoding.ASCII.GetBytes(CertContent.Text);
+            
+        bool added = wsref.AddCertificateToDatabase(certificate);
+
+            if (added)
+            {
+                System.Diagnostics.Debug.WriteLine("Certificate added!");
+                System.Diagnostics.Debug.WriteLine("Certificate Thumbprint = {0}", certificate.Thumbprint);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Certificate not added :-( ");
+            }
+
+        }
+
+
+        private void IsDeleted_OnChecked_Checked(object sender, RoutedEventArgs e)
+        {
+            {
+
+                IsDeleted.Content = "Checked";
+
+            }
+
+        }
+
+        private void IsDeleted_Unchecked(object sender, RoutedEventArgs e)
+        {
+            IsDeleted.Content = "Unchecked";
+        }
+    }
     }
