33431062d7df2c648dd08ff9ba86f461c6a6612c
will989 <will989@regis.edu>
2014-03-18 21:07:30 -0600

    Changes to cert mgr to set timeouts wsref, wsref2, wsref3

--- CertificateManagerClient/CertificateManagerPages/CompareCertificateStoreContentsPage.xaml.cs
+++ CertificateManagerClient/CertificateManagerPages/CompareCertificateStoreContentsPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -30,6 +31,8 @@ namespace CertificateManagerClient.CertificateManagerPages
             //instantiate windows service client
             //CalculatorService.CalculatorClient wsref = new CalculatorService.CalculatorClient();
             CertificateService.CertificateClient wsref = new CertificateService.CertificateClient();
+            CertificateService.CertificateClient wsref2 = new CertificateService.CertificateClient();
+            CertificateService.CertificateClient wsref3 = new CertificateService.CertificateClient();
 
             string newStoreName = null;
             string serverName = null;
@@ -93,16 +96,84 @@ namespace CertificateManagerClient.CertificateManagerPages
             System.Diagnostics.Debug.WriteLine("newStoreName = {0}", newStoreName);
             System.Diagnostics.Debug.WriteLine("storeLocation = {0}", storeLocation.ToString());
                 
+            //first, list the contents for the 2 servers:
+            try
+            {
+                log.Debug("Right before first ListCertificatesInRemoteStore -- server1");
+                wsref.InnerChannel.OperationTimeout = new TimeSpan(0, 2, 0);
+                wsref.Open();
+                var serverCertList =
+                    new List<X509Certificate2>(wsref.ListCertificatesInRemoteStore(newStoreName, storeLocation,
+                        serverName));
+                log.Debug("Right after first ListCertificatesInRemoteStore -- server1");
+                //should be greater than 0
+                int size = serverCertList.Count;
+                System.Diagnostics.Debug.WriteLine("certList count = {0}", size);
+                Server1Label.Content = serverName;
+                Server1CertDataGrid.ItemsSource = serverCertList;
+            }
+            catch (EndpointNotFoundException epnfe)
+            {
+                System.Diagnostics.Debug.WriteLine("EndpointNotFoundException caught: {0}", epnfe);
+                log.Error("Caught EndpointNotFoundException: {0}", epnfe);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception caught: {0}", ex);
+                log.Error("Caught Exception: {0}", ex);
+            }
+            finally
+            {
+                wsref.Close();
+            }
 
 
-                //DataGrid stuff based on this:  http://www.dotnetperls.com/datagrid
+
+            try
+            {
+                
+
+                //http://stackoverflow.com/questions/16716247/wcf-service-timeout-setting
+                //try setting timeout to 2 minutes
+                wsref2.InnerChannel.OperationTimeout = new TimeSpan(0, 2, 0);
+                wsref2.Open();
+                log.Debug("Right before second ListCertificatesInRemoteStore -- server2");
+                var server2CertList = new List<X509Certificate2>(wsref2.ListCertificatesInRemoteStore(newStoreName, storeLocation, serverName2));
+                log.Debug("Right after second ListCertificatesInRemoteStore -- server2");
+
+                //should be greater than 0
+                int size = server2CertList.Count;
+                System.Diagnostics.Debug.WriteLine("certList count = {0}", size);
+                Server2Label2.Content = serverName2;
+                Server2CertDataGrid.ItemsSource = server2CertList;
+            }
+            catch (EndpointNotFoundException epnfe)
+            {
+                System.Diagnostics.Debug.WriteLine("EndpointNotFoundException caught: {0}", epnfe);
+                log.Error("Caught EndpointNotFoundException: {0}", epnfe);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception caught: {0}", ex);
+                log.Error("Caught Exception: {0}", ex);
+            }
+            {
+                wsref2.Close();
+            }
+
+
+            //DataGrid stuff based on this:  http://www.dotnetperls.com/datagrid
                 //try to retrieve certificates from a remote CA
 
             try
             {
+                log.Debug("right before CompareCertificatesInStore call");
+                wsref3.InnerChannel.OperationTimeout = new TimeSpan(0, 2, 0);
+                wsref3.Open();
                 List<X509Certificate2> certList =
-                    new List<X509Certificate2>(wsref.CompareCertificatesInStore(newStoreName, storeLocation, serverName, serverName2));
-
+                    new List<X509Certificate2>(wsref3.CompareCertificatesInStore(newStoreName, storeLocation, serverName,
+                        serverName2));
+                log.Debug("right after CompareCertificatesInStore call");
                 //should be greater than 0
                 int size = certList.Count;
                 System.Diagnostics.Debug.WriteLine("certList count = {0}", size);
@@ -117,6 +188,10 @@ namespace CertificateManagerClient.CertificateManagerPages
             {
                 System.Diagnostics.Debug.WriteLine("Exception caught: {0}", ex);
                 log.Error("Caught Exception: {0}", ex);
+            }
+            finally
+            {
+                wsref3.Close();
             }
         }
 
